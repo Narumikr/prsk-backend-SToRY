26299080f58d8d84e0ec074e06088dd7d4ae6b40
Narumikr <narunarumikumiku396@gmail.com>
2025-10-16 01:54:42 +0900

    generator redoc

--- api/scripts/generate-specification.sh
+++ api/scripts/generate-specification.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
+set -e
 
 # get directory path
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 API_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 DIST_DIR=$API_DIR/dist
 
+# file name
+BUNDLED_OPENAPI="bundled-openapi.yaml"
+GEN_INDEX_HTML="index.html"
+
 # create dist directory if not exists
 mkdir -p $DIST_DIR
 
 # bundle openapi yaml
-redocly bundle $API_DIR/openapi.yaml -o $DIST_DIR/bundled-openapi.yaml
+echo "🎸 Bundling OpenAPI..."
+echo
+if ! redocly bundle $API_DIR/openapi.yaml -o $DIST_DIR/$BUNDLED_OPENAPI; then
+    echo "❌ Failed to bundle OpenAPI"
+    echo
+    exit 1
+fi
+
+echo "💫 Generated: $DIST_DIR/$BUNDLED_OPENAPI"
+echo
 
 # generate index.html
-redocly build-docs $DIST_DIR/bundled-openapi.yaml -o $DIST_DIR/index.html
+echo "🎵 Building HTML documentation..."
+echo
+if ! redocly build-docs $DIST_DIR/$BUNDLED_OPENAPI -o $DIST_DIR/$GEN_INDEX_HTML; then
+    echo "❌ Failed to build documentation"
+    echo
+    exit 1
+fi
 
-echo "✅ Generated: $DIST_DIR/bundled-openapi.yaml"
-echo "✅ Generated: $DIST_DIR/index.html"
+echo "🍀 Generated: $DIST_DIR/$GEN_INDEX_HTML"
+echo
