96f5415e965b7ec0efff4992683e5aa27bb2e0a3
Narumikr <narunarumikumiku396@gmail.com>
2025-11-29 04:05:05 +0900

    bug fix PUT /artist not implementation duplication error

--- src/main/java/com/example/untitled/artist/dto/OptionalArtistRequest.java
+++ src/main/java/com/example/untitled/artist/dto/OptionalArtistRequest.java
@@ -1,6 +1,5 @@
 package com.example.untitled.artist.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
