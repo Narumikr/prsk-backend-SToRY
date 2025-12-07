a75ba32222068ce9027888e6e7170520ea4e9597
Narumikr <narunarumikumiku396@gmail.com>
2025-12-07 23:59:01 +0900

    Add user master post api (#27)

---
+++ src/main/java/com/example/untitled/user/dto/UserRequest.java
@@ -0,0 +1,28 @@
+package com.example.untitled.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * ユーザーマスタAPIリクエスト
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+
+    /** ユーザー名 **/
+    @NotBlank(message = "ユーザー名は必須です。 - The user name is required.")
+    @Size(max = 20, message = "ユーザー名は20文字以内で入力してください。 - Please enter the user name within 20 characters.")
+    private String userName;
+
+    /** パスワード **/
+    @NotBlank(message = "パスワードは必須です。 - The password is required.")
+    @Size(max = 20, message = "パスワードは20文字以内で入力してください。 - Please enter the password with 20 characters.")
+    private String password;
+}
